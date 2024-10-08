94035391995b7213564edd8ff374d00db0dbc39a
Tan Nguyen <tannp27@gmail.com>
2024-10-08 09:18:36 +0700

    feat: add SEO content for docs homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,34 +6,42 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './home/index.module.css';
+import Head from "@docusaurus/core/lib/client/exports/Head";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/telegram-git-notifier/introduction">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+    <>
+      <Head>
+        <link rel="canonical" href="https://docs.cslant.com" data-rh="true" />
+        <meta name="description"
+              content="Documentation for CSlant projects. You can find the documentation for Laravel packages, PHP applications, and more. This documentation is easy to use and can be integrated into any project."
+              data-rh="true" />
+        <meta name="keywords"
+              content="laravel,php,package,documentation,laravel package,php application,laravel documentation,php documentation,cslant,cslant documentation"
+              data-rh="true" />
+      </Head>
+
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <Heading as="h1" className="hero__title">
+            {siteConfig.title}
+          </Heading>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link className="button button--secondary button--lg" to="/telegram-git-notifier/introduction"> Docusaurus
+              Tutorial - 5min ⏱️ </Link>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
