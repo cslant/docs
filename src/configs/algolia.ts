89d2693fd248a989187eb50cd7e8e2fbcbfca207
Tan Nguyen <tannp27@gmail.com>
2024-10-13 00:58:58 +0700

    fix: algolia config env

--- src/configs/algolia.ts
+++ src/configs/algolia.ts
@@ -3,11 +3,11 @@ require('dotenv').config();
 const AlgoliaConfig = {
   appId: process.env.ALGOLIA_APP_ID,
   apiKey: process.env.ALGOLIA_API_KEY,
-  indexName: process.env.ALGOLIA_INDEX_NAME,
+  indexName: process.env.ALGOLIA_INDEX_NAME || 'cslant',
   contextualSearch: true,
   externalUrlRegex: 'external\\.com|domain\\.com',
   searchParameters: {},
-  searchPagePath: process.env.ALGOLIA_SEARCH_PAGE_PATH,
+  searchPagePath: process.env.ALGOLIA_SEARCH_PAGE_PATH || 'search',
 };
 
 export default AlgoliaConfig;
