55f47244f4ae44aed87f652670eec592469760b3
Tan Nguyen <tannp27@gmail.com>
2025-02-17 23:46:36 +0700

    feat: add GitHub project PHP documentation static directory

--- src/configs/staticDirectories.ts
+++ src/configs/staticDirectories.ts
@@ -1,4 +1,5 @@
 export const RepoDocsStatic = [
   'repos/telegram-git-notifier-docs/assets/public',
   'repos/laravel-like-docs/assets/public',
+  'repos/github-project-php-docs/assets/public',
 ];
